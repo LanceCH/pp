f240cfed8c0dbe25c6c4f3d4e4b75ae4261fc92f
zhangjunrao <zhangjunrao@qq.com>
2018-04-14 21:29:36 +0800

    up

--- src/views/home/home.ts
+++ src/views/home/home.ts
@@ -1,7 +1,7 @@
 import Vue from 'util/vueExt'
 import { Component } from 'vue-property-decorator'
 import Template from './home.vue'
-
+const echarts = require('echarts')
 @Component({
   mixins: [Template]
 })
@@ -26,4 +26,183 @@ export default class Home extends Vue {
       text: '应用6'
     }
   ]
+
+  activeName1: string = '1'
+  activeName2: string = '1'
+
+  option1 = {
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} ({d}%)'
+    },
+    legend: {
+      orient: 'vertical',
+      x: 'left',
+      data: ['产品一', '产品二', '产品三', '产品四', '产品五']
+    },
+    series: [
+      {
+        name: '访问来源',
+        type: 'pie',
+        radius: ['50%', '70%'],
+        avoidLabelOverlap: false,
+        label: {
+          normal: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            show: true,
+            textStyle: {
+              fontSize: '30',
+              fontWeight: 'bold'
+            }
+          }
+        },
+        labelLine: {
+          normal: {
+            show: false
+          }
+        },
+        data: [
+          { value: 335, name: '产品一' },
+          { value: 310, name: '产品二' },
+          { value: 234, name: '产品三' },
+          { value: 135, name: '产品四' },
+          { value: 1548, name: '产品五' }
+        ]
+      }
+    ]
+  }
+
+  option2 = {
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} ({d}%)'
+    },
+    legend: {
+      orient: 'vertical',
+      x: 'left',
+      data: ['产品一', '产品二', '产品三', '产品四', '产品五']
+    },
+    series: [
+      {
+        name: '访问来源',
+        type: 'pie',
+        radius: ['50%', '70%'],
+        avoidLabelOverlap: false,
+        label: {
+          normal: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            show: true,
+            textStyle: {
+              fontSize: '30',
+              fontWeight: 'bold'
+            }
+          }
+        },
+        labelLine: {
+          normal: {
+            show: false
+          }
+        },
+        data: [
+          { value: 335, name: '产品一' },
+          { value: 310, name: '产品二' },
+          { value: 234, name: '产品三' },
+          { value: 135, name: '产品四' },
+          { value: 1548, name: '产品五' }
+        ]
+      }
+    ]
+  }
+
+  option3 = {
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} ({d}%)'
+    },
+    legend: {
+      orient: 'vertical',
+      x: 'left',
+      data: ['产品一', '产品二', '产品三']
+    },
+    series: [
+      {
+        name: '访问来源',
+        type: 'pie',
+        radius: ['50%', '70%'],
+        avoidLabelOverlap: false,
+        label: {
+          normal: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            show: true,
+            textStyle: {
+              fontSize: '30',
+              fontWeight: 'bold'
+            }
+          }
+        },
+        labelLine: {
+          normal: {
+            show: false
+          }
+        },
+        data: [
+          { value: 335, name: '产品一' },
+          { value: 310, name: '产品二' },
+          { value: 234, name: '产品三' }
+        ]
+      }
+    ]
+  }
+
+  option = [this.option1, this.option2, this.option3]
+  myChart = null
+
+  tableData = [
+    {
+      date: '2016-05-02',
+      name: '产品1',
+      num: '100'
+    },
+    {
+      date: '2016-05-04',
+      name: '产品2',
+      num: '100'
+    },
+    {
+      date: '2016-05-01',
+      name: '产品3',
+      num: '100'
+    },
+    {
+      date: '2016-05-03',
+      name: '产品4',
+      num: '100'
+    }
+  ]
+
+  handleClick(tab, event) {
+    // 绘制图表
+    this.myChart.setOption(this.option[Number(tab.name) - 1])
+  }
+
+  handleClickTable(tab, event) {
+
+  }
+
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    this.myChart = (echarts as any).init(
+      document.getElementById('echartContainer')
+    )
+    this.myChart.setOption(this.option[0])
+  }
 }
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -15,6 +15,54 @@
         </ul>
       </div>
     </div>
+    <div class="charts">
+      <div class="content">
+        <div class="header">
+          <h1>销售额类别占比</h1>
+          <el-tabs v-model="activeName1" @tab-click="handleClick">
+            <el-tab-pane label="全部渠道" name="1"></el-tab-pane>
+            <el-tab-pane label="门店" name="2"></el-tab-pane>
+            <el-tab-pane label="线上" name="3"></el-tab-pane>
+          </el-tabs>
+        </div>
+        <div class="echartContainer" id="echartContainer"></div>
+      </div>
+      <div class="content">
+          <div class="header">
+            <h1>销售额排名一览</h1>
+            <el-tabs v-model="activeName2" @tab-click="handleClickTable">
+              <el-tab-pane label="全部渠道" name="1"></el-tab-pane>
+              <el-tab-pane label="门店" name="2"></el-tab-pane>
+              <el-tab-pane label="线上" name="3"></el-tab-pane>
+            </el-tabs>
+          </div>
+          <div class="table">
+            <el-table
+                :data="tableData"
+                stripe
+                style="width: 100%">
+                <el-table-column
+                  prop="name"
+                  label="产品名称"
+                  width="180">
+                </el-table-column>
+                <el-table-column
+                  prop="num"
+                  label="访问量">
+                </el-table-column>
+                <el-table-column
+                  prop="date"
+                  label="日期"
+                  width="180">
+                </el-table-column>
+              </el-table>
+              <el-pagination
+                layout="prev, pager, next"
+                :total="50">
+              </el-pagination>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 <style src="./home.scss" lang="scss" scoped></style>
